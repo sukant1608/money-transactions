14f807d4b18c84c7410c4eed2a44ac856cff928e
sukant1608 <53792814+sukant1608@users.noreply.github.com>
2021-06-17 14:35:16 +0530

    Heroku config

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ app.use(cors());
 const handle = require("./handlers");
 const routes = require("./routes");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
+
+if (process.NODE_ENV === "production") {
+  app.use(express.static("client/build/"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, function () {
   console.log(`Server is running on port ${port}`);
